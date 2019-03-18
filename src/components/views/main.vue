b80edac358b05f6b87876422ad5030d6a6ffa90d
liuchao <liuchao102@163.com>
2019-03-18 22:53:16 +0800

    1

--- src/components/views/main.vue
+++ src/components/views/main.vue
@@ -112,6 +112,7 @@
                             this.num = this.$store.state.playIndex;
                             let audio = document.getElementById('audio');
                             document.addEventListener("WeixinJSBridgeReady", function () {
+                                alert(1)
                                 audio.play();
                             }, false);
                             this.listen()
